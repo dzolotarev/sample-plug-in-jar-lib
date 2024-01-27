c9e2d788e7b818d2f7239d31217645f2c5a4b30f
Denis Zolotarev <judaeus@gmail.com>
2024-01-27 17:40:17 +0200

    * init commit

---
+++ another-project/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.add(1, 2));
+        System.out.println(calculator.subtract(5, 3));
+    }
+
+
+}
